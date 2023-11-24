6c790c82cbc1b731876d7f3aa4d362208c111e7c
Takahito Nakano <kano@ponko2.jp>
2023-11-24 22:13:15 +0900

    Submit practice A

--- practice/a/main.cpp
+++ practice/a/main.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 
 int main() {
-  int N;
-  cin >> N;
+  int a;
+  cin >> a;
+
+  int b, c;
+  cin >> b >> c;
+
+  string s;
+  cin >> s;
+
+  cout << (a + b + c) << " " << s << endl;
 }
