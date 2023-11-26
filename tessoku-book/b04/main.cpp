cc4060b143f876daa46c188046f82f2c00ac40da
Takahito Nakano <kano@ponko2.jp>
2023-11-26 20:00:59 +0900

    Submit tessoku-book B04

--- tessoku-book/b04/main.cpp
+++ tessoku-book/b04/main.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 
 int main() {
-  int N;
+  string N;
   cin >> N;
+
+  int size = N.size();
+  int answer = 0;
+  for (int i = 0; i < size; i++) {
+    if (N.at(i) == '1') {
+      answer += 1 << (size - i - 1);
+    }
+  }
+
+  cout << answer << endl;
 }
