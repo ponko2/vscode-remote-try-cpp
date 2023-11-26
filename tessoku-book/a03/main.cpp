1476f3d62b6442f5a2f9fec404d3167eb809d744
Takahito Nakano <kano@ponko2.jp>
2023-11-26 18:00:40 +0900

    Submit tessoku-book A03

--- tessoku-book/a03/main.cpp
+++ tessoku-book/a03/main.cpp
@@ -2,6 +2,27 @@
 using namespace std;
 
 int main() {
-  int N;
-  cin >> N;
+  int N, K;
+  cin >> N >> K;
+
+  vector<int> P(N), Q(N);
+
+  for (int i = 0; i < N; i++) {
+    cin >> P.at(i);
+  }
+
+  for (int i = 0; i < N; i++) {
+    cin >> Q.at(i);
+  }
+
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      if (P.at(i) + Q.at(j) == K) {
+        cout << "Yes" << endl;
+        return 0;
+      }
+    }
+  }
+
+  cout << "No" << endl;
 }
