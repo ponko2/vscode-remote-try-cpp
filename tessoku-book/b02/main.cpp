0794b63833a83f474d50b206ebd77942a67a6516
Takahito Nakano <kano@ponko2.jp>
2023-11-26 17:44:45 +0900

    Submit tessoku-book B02

--- tessoku-book/b02/main.cpp
+++ tessoku-book/b02/main.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 
 int main() {
-  int N;
-  cin >> N;
+  int A, B;
+  cin >> A >> B;
+
+  for (int i = A; i <= B; i++) {
+    if (100 % i == 0) {
+      cout << "Yes" << endl;
+      return 0;
+    }
+  }
+
+  cout << "No" << endl;
 }
