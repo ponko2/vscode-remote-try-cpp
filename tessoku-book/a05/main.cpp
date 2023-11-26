2f81ef6beb74c5b9813f0e18e1c0c6fdc6a22741
Takahito Nakano <kano@ponko2.jp>
2023-11-26 21:06:42 +0900

    Submit tessoku-book A05

--- tessoku-book/a05/main.cpp
+++ tessoku-book/a05/main.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 
 int main() {
-  int N;
-  cin >> N;
+  int N, K;
+  cin >> N >> K;
+
+  int answer = 0;
+  for (int x = 1; x <= N; x++) {
+    for (int y = 1; y <= N; y++) {
+      int z = (K - x - y);
+      if (z >= 1 && z <= N) {
+        answer++;
+      }
+    }
+  }
+
+  cout << answer << endl;
 }
